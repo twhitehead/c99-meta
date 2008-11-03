0d9b390047d2351f515cc422b914561263fa4609
Tyson Whitehead <twhitehead@gmail.com>
2008-11-03 16:54:58 -0500

    Combine the type and closure in Return for consistency

--- Reduce.h
+++ Reduce.h
@@ -33,11 +33,12 @@
 //       This is like the STG's function application.  Except where it is
 //       desired to go through the recursion system, invoke f directly instead.
 //
-//   Return(t,tc,__VA_ARGS__)
+//   Return(x,__VA_ARGS__)
 //
 //     - Return the results of an evaluation (pop the continuation off the top
-//       of the stack, push tc and t onto the the stack, and tail call the
-//       continuation).  This is like the STG's constructor application.
+//       of the stack, pushing the type and closure for x onto the the stack,
+//       and tail call the continuation).  This is like the STG's constructor
+//       application.
 //
 //   Output(o,f,__VA_ARGS__)
 //
@@ -52,6 +53,8 @@
 // and duplicating some reductions is much less).
 
 #define _reduce_expand(...) __VA_ARGS__
+#define _reduce_first(x,...) x
+#define _reduce_rest(x,...)  (__VA_ARGS__)
 
 
 #define reduce_eval(...)      recurse_recurse(__VA_ARGS__,reduce_eval0)
@@ -61,37 +64,37 @@
 
 #define reduce_Force1(f,x0,...)        _reduce_Force10(f,,x0,__VA_ARGS__)
 #define _reduce_Force10(f,ft,x0,...)   Recurse,(_reduce_expand x0,reduce_Force11,f,ft,__VA_ARGS__)
-#define _reduce_Force11(...)           _reduce_Force12(__VA_ARGS__)
-#define _reduce_Force12(t,tc,f,ft,...) _##f##_##t##ft (_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force11(t,tc,f,ft,...) _reduce_Force12(f,ft,t,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force12(f,ft,t,...)    _##f##_##t##ft (__VA_ARGS__)
 
 #define reduce_Force2(f,x0,x1,...)        _reduce_Force20(f,,x1,x0,__VA_ARGS__)
 #define _reduce_Force20(f,ft,x1,...)      Recurse,(_reduce_expand x1,reduce_Force21,f,ft,__VA_ARGS__)
-#define _reduce_Force21(...)              _reduce_Force22(__VA_ARGS__)
-#define _reduce_Force22(t,tc,f,ft,x0,...) _reduce_Force10(f,t##ft,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force21(t,tc,f,ft,x0,...) _reduce_Force22(f,ft,t,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force22(f,ft,t,...)       _reduce_Force10(f,t##ft,__VA_ARGS__)
 
 #define reduce_Force3(f,x0,x1,x2,...)        _reduce_Force30(f,,x2,x1,x0,__VA_ARGS__)
 #define _reduce_Force30(f,ft,x2,...)         Recurse,(_reduce_expand x2,reduce_Force31,f,ft,__VA_ARGS__)
-#define _reduce_Force31(...)                 _reduce_Force32(__VA_ARGS__)
-#define _reduce_Force32(t,tc,f,ft,x1,x0,...) _reduce_Force20(f,t##ft,x1,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force31(t,tc,f,ft,x1,x0,...) _reduce_Force32(f,ft,t,x1,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force32(f,ft,t,...)          _reduce_Force20(f,t##ft,__VA_ARGS__)
 
 #define reduce_Force4(f,x0,x1,x2,x3,...)        _reduce_Force40(f,,x3,x2,x1,x0,__VA_ARGS__)
 #define _reduce_Force40(f,ft,x3,...)            Recurse,(_reduce_expand x3,reduce_Force41,f,ft,__VA_ARGS__)
-#define _reduce_Force41(...)                    _reduce_Force42(__VA_ARGS__)
-#define _reduce_Force42(t,tc,f,ft,x2,x1,x0,...) _reduce_Force30(f,t##ft,x2,x1,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force41(t,tc,f,ft,x2,x1,x0,...) _reduce_Force42(f,ft,t,x2,x1,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force42(f,ft,t,...)             _reduce_Force30(f,t##ft,__VA_ARGS__)
 
 #define reduce_Force5(f,x0,x1,x2,x3,x4,...)        _reduce_Force50(f,,x4,x3,x2,x1,x0,__VA_ARGS__)
 #define _reduce_Force50(f,ft,x4,...)               Recurse,(_reduce_expand x3,reduce_Force51,f,ft,__VA_ARGS__)
-#define _reduce_Force51(...)                       _reduce_Force52(__VA_ARGS__)
-#define _reduce_Force52(t,tc,f,ft,x3,x2,x1,x0,...) _reduce_Force40(f,t##ft,x3,x2,x1,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force51(t,tc,f,ft,x3,x2,x1,x0,...) _reduce_Force52(f,ft,t,x3,x2,x1,x0,_reduce_expand tc,__VA_ARGS__)
+#define _reduce_Force52(f,ft,t,...)                _reduce_Force40(f,t##ft,__VA_ARGS__)
 
 
-#define reduce_Return(t,tc,f,...) Recurse,(f,t,tc,__VA_ARGS__)
+#define reduce_Return(x,f,...) Recurse,(f,_reduce_first x,_reduce_rest x,__VA_ARGS__)
 
-#define reduce_Apply(f,...)       Recurse,(f,__VA_ARGS__)
+#define reduce_Apply(f,...)    Recurse,(f,__VA_ARGS__)
 
-#define reduce_Output(o,f,...)    Output,o,(f,__VA_ARGS__)
+#define reduce_Output(o,f,...) Output,o,(f,__VA_ARGS__)
 
-#define reduce_Error(o,...)       Quit,("ERROR: "#o)
+#define reduce_Error(o,...)    Quit,("ERROR: "#o)
 
 
 #ifndef REDUCE_QUALIFIED_ONLY
