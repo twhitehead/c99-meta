30b259e3baa375d994453c93a4e75ac727729400
Tyson Whitehead <twhitehead@gmail.com>
2008-12-03 11:58:31 -0500

    Add the zero tupple, constructor shortcuts, and unqualified names

--- Tupple.h
+++ Tupple.h
@@ -12,10 +12,18 @@
 #define TUPPLE_H_INCLUDED
 
 
-#define _tupple_Tupple1(x0,...)             reduce_construct((tupple_Tupple1,x0),            __VA_ARGS__)
-#define _tupple_Tupple2(x0,x1,...)          reduce_construct((tupple_Tupple2,x0,x1),         __VA_ARGS__)
-#define _tupple_Tupple3(x0,x1,x2,...)       reduce_construct((tupple_Tupple3,x0,x1,x2),      __VA_ARGS__)
-#define _tupple_Tupple4(x0,x1,x2,x3,...)    reduce_construct((tupple_Tupple4,x0,x1,x2,x3),   __VA_ARGS__)
+#define tupple_T0                 (tupple_Tupple0)
+#define tupple_T1(x0)             (tupple_Tupple1,x0)
+#define tupple_T2(x0,x1)          (tupple_Tupple2,x0,x1)
+#define tupple_T3(x0,x1,x2)       (tupple_Tupple3,x0,x1,x2)
+#define tupple_T4(x0,x1,x2,x3)    (tupple_Tupple4,x0,x1,x2,x3)
+#define tupple_T5(x0,x1,x2,x3,x4) (tupple_Tupple5,x0,x1,x2,x3,x4)
+
+#define _tupple_Tupple0(...)                reduce_construct((tupple_Tupple0)               ,__VA_ARGS__)
+#define _tupple_Tupple1(x0,...)             reduce_construct((tupple_Tupple1,x0)            ,__VA_ARGS__)
+#define _tupple_Tupple2(x0,x1,...)          reduce_construct((tupple_Tupple2,x0,x1)         ,__VA_ARGS__)
+#define _tupple_Tupple3(x0,x1,x2,...)       reduce_construct((tupple_Tupple3,x0,x1,x2)      ,__VA_ARGS__)
+#define _tupple_Tupple4(x0,x1,x2,x3,...)    reduce_construct((tupple_Tupple4,x0,x1,x2,x3)   ,__VA_ARGS__)
 #define _tupple_Tupple5(x0,x1,x2,x3,x4,...) reduce_construct((tupple_Tupple5,x0,x1,x2,x3,x4),__VA_ARGS__)
 
 
@@ -30,5 +38,21 @@
 
 #define _tupple_uncurry(f,x0,x1,...) reduce_return(f,(tupple_Tupple2,x0,x1),__VA_ARGS__)
 
+
+#ifndef TUPPLE_QUALIFIED_ONLY
+  #define T0 tupple_T0
+  #define T1 tupple_T1
+  #define T2 tupple_T2
+  #define T3 tupple_T3
+  #define T4 tupple_T4
+  #define T5 tupple_T5
+
+  #define Tupple0 tupple_Tupple0
+  #define Tupple1 tupple_Tupple1
+  #define Tupple2 tupple_Tupple2
+  #define Tupple3 tupple_Tupple3
+  #define Tupple4 tupple_Tupple4
+  #define Tupple5 tupple_Tupple5
+#endif // TUPPLE_QUALIFIED_ONLY
 
 #endif // TUPPLE_H_INCLUDED
