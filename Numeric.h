adab059bbb3145defd5a43b032986f475d010b24
Tyson Whitehead <twhitehead@gmail.com>
2017-11-16 16:14:26 -0500

    Switch to the LGPL for a slightly more permissive license

--- Numeric.h
+++ Numeric.h
@@ -2,9 +2,9 @@
 // Copyright (C) 2008 Tyson Whitehead
 //
 // This code is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published
-// by the Free Software Foundation; either version 2, or (at your
-// option) any later version.
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2, or (at
+// your option) any later version.
 //
 //---------------------------------------------------------------------------//
 
